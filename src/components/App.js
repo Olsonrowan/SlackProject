84956feb5897b076f85ef3bfdfa6b1c3aa895707
Olsonrowan <56852849+Olsonrowan@users.noreply.github.com>
2020-07-06 19:44:59 -0600

    added desgin to signup and better auth
    added desgin to signup and better auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,58 +1,121 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import firebase from 'firebase'
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+import { 
+    BrowserRouter as Router,
+    Route,
+    NavLink 
+} from 'react-router-dom'
+import firebase, { db, auth } from '../services/firebase'
 
 
-firebase.initializeApp({
-    apiKey: "AIzaSyA15xf-hsjSbgCBV4yy_3HWJtPO-2qrbjo",
-    authDomain: "classchatter-83971.firebaseapp.com"
-})
+// import { signout } from '../services/auth'
+
+import PrivateRoute from './PrivateRoute'
 
 
-class App extends Component{
-    state = { isSignedIn: false }
-    uiConfig = {
-        signInFlow: "popup",
-        signInOptions: [
-            firebase.auth.EmailAuthProvider.PROVIDER_ID,
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-            firebase.auth.GithubAuthProvider.PROVIDER_ID,
-        ],
-        callbacks: {
-            signInSuccess: () => false
-        }
-    }
+import LoginPage from '../pages/LoginPage';
+import SignUpPage from '../pages/SignUpPage'
+// import { AuthProvider } from '../services/auth';
+import HomePage from '../pages/HomePage'
 
-    componentDidMount = () =>{
-        firebase.auth().onAuthStateChanged(user => {
-            this.setState({isSignedIn: !!user})
-            console.log("user",user)
-        })
 
-    }
-
+class App extends React.Component{
     render(){
-        return (
-            <div>
-                {this.state.isSignedIn ? (
-                <span>
-                  <div>You Are Signed in!</div>
-                  <button onClick={()=> firebase.auth().signOut()}>Sign Out!</button>
-                  <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
-                  <img
-                   alt="profile picture" 
-                   src={firebase.auth().currentUser.photoURL}
-                   />
-                </span>
-                ) : (
-                <StyledFirebaseAuth uiConfig={this.uiConfig}
-                firebaseAuth={firebase.auth()} />
-                 )}
-            </div>
+        return(
+        <div>
+            <Router>
+                {/* <Route exact path='/'>
+                    <HomePage ={} />
+                </Route> */}
+                <Route  path='/login' component={LoginPage}/>
+                <Route  path='/signup' component={SignUpPage}/>
 
+            </Router>
+        </div>
         )
     }
 }
 
+
 export default App
+
+
+
+
+
+// const App = () =>{
+    
+//         return (
+//             <AuthProvider>
+//             <Router>
+               
+//                     <div>
+//                     <PrivateRoute exact path="/" component={ HomePage } />
+//                     <Route path="/login" component={ LoginPage } />
+//                     <Route path="/signup" component={ SignUpPage } />
+
+//                     </div>
+//             </Router>
+//             </AuthProvider>
+//         )
+// }
+
+// class App extends React.Component{
+//     state ={
+//         user: null
+//     }
+
+//     componentDidMount(){
+//         auth().onAuthStateChanged((user) => {
+//             if (user) {
+//                 this.setState({
+//                     user: {
+//                         photoURL: user.photoURL,
+//                         displayName: user.displayName,
+//                     }
+//                 })
+//             } else {
+//               // No user is signed in.
+//               this.setState({ user: null })
+//             }
+//           });
+//     }
+
+//     signInUser = () =>{
+//         const provider = new firebase.auth.GoogleAuthProvider();
+//         auth().signInWithPopup(provider)
+//         .then((result) => {
+//             var user = result.user;
+//             console.log(user)
+//           }).catch(function(error) {
+//            console.log(error)
+//           });
+
+//     }
+//     signOutUser(){
+//         auth().signOut()
+//     }   
+//     render(){ 
+//         return(
+//         <div className="App">
+//             <Router>
+//                 <Nav signout={this.signOutUser} user={this.state.user} />
+//                 <Route exact path="/" component={Home} />
+//                 <Route path="/login" component={ () => <Login signin={this.signInUser} />} />
+//                 <Route path="/signup" component={Signup} />
+//                 <Route path='/profile' component={() => <Profile user={this.state.user} /> } />
+
+//             </Router>
+//             { this.state.user &&
+//             <div>
+//                 <h2>{this.state.user.displayName}</h2>
+//                 <img alt="profilepic" src={this.state.user.photoURL} />
+//             </div>
+//             }
+//         </div>
+//     )
+//   }
+// }
+
+
+
+// export default App
