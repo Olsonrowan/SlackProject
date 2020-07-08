b748998a0b240f116b758ec389be05cd61f69d4b
Olsonrowan <56852849+Olsonrowan@users.noreply.github.com>
2020-07-08 12:04:08 -0600

    routing error fix
    routing error fix

--- src/index.js
+++ src/index.js
@@ -3,28 +3,39 @@ import ReactDOM from 'react-dom';
 import App from './components/App'
 import {BrowserRouter as Router, Switch, Route, withRouter} from 'react-router-dom'
 import firebase from './services/firebase'
+
+//pages
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 
+//loading
+import Spinner from './components/Spinner'
 
 //redux
 import {createStore} from 'redux'
-import { Provider } from 'react-redux'
+import { Provider, connect } from 'react-redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
-const store = createStore(()=>{}, composeWithDevTools());
+//actions and reducers
+import rootReducer from './reducers';
+import { setUser } from './actions/index'
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 
 class Root extends React.Component{
   componentDidMount(){
+    console.log(this.props.isLoading)
     firebase.auth().onAuthStateChanged(user =>{
       if(user){
+        this.props.setUser(user);
         this.props.history.push('/');
       }
     })
   }
   render(){
-    return(
+    return this.props.isLoading ? <Spinner /> : (
+    
         <Switch>
           <Route path="/" component={App} />
           <Route path="/login" component={LoginPage} />
@@ -35,16 +46,17 @@ class Root extends React.Component{
   }
 }
 
-const RootWithAuth = withRouter(Root);
+const mapStateFromProps = state => ({
+  isLoading: state.user.isLoading
+})
+
+const RootWithAuth = withRouter(connect(mapStateFromProps, { setUser })(Root));
 
 ReactDOM.render(
-    <React.StrictMode>
     <Provider store={store}>
       <Router>
           <RootWithAuth />
       </Router>
-    </Provider>
-      
-    </React.StrictMode>,
-    document.getElementById('root')
+    </Provider>,
+          document.getElementById('root')
   );
